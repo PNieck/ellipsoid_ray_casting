667495a13af5abab5e728fb352867bfd21228afb
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2024-02-25 14:22:48 +0100

    Draw empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+
+fn main() -> Result<(), winit::error::EventLoopError> {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                elwt.exit();
+            },
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+                window.request_redraw();
+            },
+            _ => ()
+        }
+    })
 }
